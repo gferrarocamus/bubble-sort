78077aacc9561c0237a5bb306cd372f493ee4187
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-03-21 10:50:44 +0100

    updates second sorting method

--- main.rb
+++ main.rb
@@ -1,4 +1,3 @@
-=begin
 def bubble_sort(arr)
 
     (arr.length-1).times do
@@ -11,28 +10,18 @@ def bubble_sort(arr)
             end
         end    
     end
-
     return arr
 end
-
-=end
 
 def bubble_sort_by(arr)
     (arr.length-1).times do
         arr.each_with_index do |item, i|
-            yield(item, arr[i+1])
+            if i < arr.length-1
+                if yield(item, arr[i+1]) > 0
+                    arr[i] = arr[i+1]
+                    arr[i+1] = item
+                end
+            end
         end    
     end
 end
-
-bubble_sort_by([4,2,6,19,3,1,20]) { |x,y| x, y = y, x if x > y  }
-
-
-
-#test 
-#bubble_sort(test)
-#bubble_sort_by(test)
-
-test.each {|item| puts item}
-
-
